f21ad7baab0169818b839203ecb3e1ff3773ac13
Larissa Samara <larissasamaraxavier@gmail.com>
2024-05-09 18:20:36 -0300

    create vector

---
+++ vector/IVector.java
@@ -0,0 +1,8 @@
+public interface IVector {
+  public Object elementAtRank(int rank);
+  public Object replaceAtRank( int rank, Object element);
+  public void insertAtRank(int rank, Object element);
+  public Object removeAtRank(int rank);
+  public Boolean isEmpty();
+  public Integer size();
+}
---
+++ vector/Vector.java
@@ -0,0 +1,61 @@
+public class Vector implements IVector {
+  private Object[] vector;
+	private int size;
+	private int quantity = 0;
+
+  public Vector(int size){
+    size = size;
+    vector = new Object[size];
+  }
+  
+  public Object elemAtRank(int rank) {
+		return vector[rank];
+	}
+
+  public Object replaceAtRank(int rank, Object element) {
+		Object temp = vector[rank];
+		vector[rank] = element;
+		return temp;
+	}
+
+  public void insertAtRank(int rank, Object element) {
+
+		if (quantity == size) {
+			size *= 2;
+			Object[] newVector = new Object[size];
+			for (int i = 0; i < quantity; i++) {
+				newVector[i] = vector[i];
+			}
+			vector = newVector;
+		} else {
+			/* ele percorre do final até a posição que quero inserir movendo os elementos */
+			for (int i = quantity - 1; i >= rank; i--) {
+				vector[i + 1] = vector[i];
+			}
+			vector[size] = e;
+			quantity++;
+		}
+	}
+
+  public Object removeAtRank(int rank) {
+		if (!(rank >= 0 && rank < size)) {
+			System.out.println("Posição não existe");
+		}
+		Object temp = vector[rank];
+		for (int i = rank; i < size - 1; i++){
+			// removendo e realocando os elementos voltando 1.
+			vector[i] = vector[i + 1];			
+		}					
+		quantity--;
+		return temp;
+	}
+
+  public int sizeInsert() {
+		return quantity;
+	}
+
+  public boolean isEmpty() {
+		return sizeInsert() == 0;
+	}
+
+}
